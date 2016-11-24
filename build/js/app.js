5536743eb87f7ed6078b43b30ecfb0f835e11818
NazariyM <nazarriko@gmail.com>
2016-11-24 11:39:40 +0200

    done

--- build/js/app.js
+++ build/js/app.js
@@ -26,15 +26,15 @@ $(document).ready(function() {
 
 	(function() {
 
-		var selectDefault = $('.js-select-default'),
-			select = $('.js-select');
+		var select = $('.js-select');
 
-		selectDefault.after('<div class="custom-select js-custom-select"></div>');
-		selectDefault.after('<div class="current-option js-current-option">' + 'Город*' + '</div>');
+		select.wrap("<div class='select-style'></div>");
+		select.after('<div class="custom-select js-custom-select"></div>');
+		select.after('<div class="current-option js-current-option">' + 'Город*' + '</div>');
 		var customSelect = $('.js-custom-select'),
 			currentOption = $('.js-current-option');
 
-		selectDefault.children().each(function() {
+		select.children().each(function() {
 			customSelect.append('<div class="custom-select__option">' + $(this).val() + '</div>');
 		});
 
@@ -45,15 +45,15 @@ $(document).ready(function() {
 
 		customSelect.children().on('click', function() {
 			$(this).addClass('is-active').siblings().removeClass('is-active');
-			selectDefault.children().eq($(this).index()).attr('selected', true).siblings().removeAttr('selected');
+			select.children().eq($(this).index()).attr('selected', true).siblings().removeAttr('selected');
 			currentOption.text($(this).text()).addClass('is-selected');
 			$(this).parent().slideUp();
 		});
 
 		$(document).mouseup(function(e) {
-			if (!select.is(e.target)
+			if (!currentOption.is(e.target)
 				&&
-				select.has(e.target).length === 0)
+				currentOption.has(e.target).length === 0)
 			{
 				customSelect.slideUp();
 			}
