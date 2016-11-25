9f434c4a0664476821955c2d736b38da95c51d7d
NazariyM <nazarriko@gmail.com>
2016-11-25 10:29:48 +0200

    finish

--- src/js/common.js
+++ src/js/common.js
@@ -1,49 +1,49 @@
 $(document).ready(function() {
 
-    (function() {
+	(function() {
 
-        var select = $('.js-select');
+		var select = $('.js-select');
 
-        select.wrap("<div class='select-style'></div>");
-        select.after('<div class="custom-select js-custom-select"></div>');
-        select.after('<div class="current-option js-current-option">' + 'Город*' + '</div>');
+		select.wrap('<div class="select-style"></div>');
+		select.after('<div class="custom-select js-custom-select"></div>');
+		select.after('<div class="current-option js-current-option">' + 'Город*' + '</div>');
 
 
-        select.each(function() {
+		select.each(function() {
 			  $(this).children().each(function() {
-	            $(this).parent().siblings('.js-custom-select').append('<div class="custom-select__option">' + $(this).val() + '</div>');
-	        });
+				$(this).parent().siblings('.js-custom-select').append('<div class="custom-select__option">' + $(this).text() + '</div>');
+			});
 
-            var currentOption = $(this).next(),
-                customSelect = currentOption.next();
+			var currentOption = $(this).next(),
+				customSelect = currentOption.next();
 
-            currentOption.on('click', function() {
+			currentOption.on('click', function() {
 					if ($(this).parent().hasClass('is-active')) {
 						customSelect.slideUp(function() {
 							$(this).parent().removeClass('is-active');
 						});
 					} else {
-						$(this).parent().addClass('is-priora');
+						$(this).parent().addClass('is-priority');
 						$(this).parent().addClass('is-active');
 						customSelect.slideDown(function(){
-							$(this).parent().removeClass('is-priora');
+							$(this).parent().removeClass('is-priority');
 						});
 					}
-                $(this).toggleClass('is-active');
-            });
+				$(this).toggleClass('is-active');
+			});
 
-            customSelect.children().on('click', function() {
-                var _this = $(this),
-                    index = _this.index(),
-                    text = _this.text();
+			customSelect.children().on('click', function() {
+				var _this = $(this),
+					index = _this.index(),
+					text = _this.text();
 
-                _this.addClass('is-active').siblings().removeClass('is-active');
-                _this.parent().siblings(select).children().eq(index).attr('selected', true).siblings().removeAttr('selected');
-                currentOption.text(text).addClass('is-selected');
-                _this.parent().slideUp(function () {
-                	currentOption.parent().removeClass('is-active');
-                });
-            });
+				_this.addClass('is-active').siblings().removeClass('is-active');
+				_this.parent().siblings(select).children().eq(index).attr('selected', true).siblings().removeAttr('selected');
+				currentOption.text(text).addClass('is-selected');
+				_this.parent().slideUp(function () {
+					currentOption.parent().removeClass('is-active');
+				});
+			});
 				$(document).click(function(e) {
 				if (!currentOption.is(e.target) && currentOption.has(e.target).length === 0) {
 					currentOption.removeClass('is-active');
@@ -53,6 +53,6 @@ $(document).ready(function() {
 
 				}
 			});
-        });
-    })();
+		});
+	})();
 });
